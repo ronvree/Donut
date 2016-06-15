4b36b1a120a185ba51e568879d6c4b0aa060e392
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-06-15 16:31:22 +0200

    Not every test is succeeding yet

--- src/donut/GrammarTest.java
+++ src/donut/GrammarTest.java
@@ -13,47 +13,97 @@ import java.util.List;
  */
 public class GrammarTest {
 
-    private static final String FILE = "src/donut/sample/sample1.donut";
+    /**
+     * Test files with a full program.
+     */
+    private static final String EmtpyFile = "src/donut/sample/test/testEmpty.donut";
+    private static final String IfStatementsFile = "src/donut/sample/test/testIfStatements.donut";
+    private static final String MargeFile = "src/donut/sample/test/testMarge.donut";
+    private static final String NumberOperatorsFile = "src/donut/sample/test/testNumberOperators.donut";
+    private static final String NumbersFile = "src/donut/sample/test/testNumbers.donut";
+    private static final String ReactionFile = "src/donut/sample/test/testReaction.donut";
+    private static final String ReactionOperatorsFile = "src/donut/sample/test/testReactionOperators.donut";
 
+
+    /**
+     * Tests for certain parts of the code.
+     */
     private static final String IntegerDeclaration = "number n = 4;";
+    private static final String IntegerDeclaration2 = "number j;";
+    private static final String ArrayDeclaration = "bunchof number a = bunchof 6 number;";
+    private static final String CharacterDeclaration = "symbol c = \'%\';";
+    private static final String CharacterDeclaration2 = "symbol g;";
+    private static final String BooleanDeclaration = "reaction r = WOOHOO;";
+    private static final String BooleanDeclaration2 = "reaction d;";
 
     @Test
     public void testArrayDeclaration() {
-
+        verify(runTest(format(ArrayDeclaration), false));
+        // TODO test arraydeclaration without assigning.
     }
 
     @Test
     public void testCharacterDeclaration() {
-
+        verify(runTest(format(CharacterDeclaration), false));
+        verify(runTest(format(CharacterDeclaration2), false));
     }
 
     @Test
     public void testBooleanDeclaration() {
+        verify(runTest(format(BooleanDeclaration), false));
+        verify(runTest(format(BooleanDeclaration2), false));
     }
 
     @Test
     public void testIntegerDeclaration() {
-        List<String> errors = runTest(format(IntegerDeclaration), false);
-        Assert.assertTrue(errors.size() == 0);
+        verify(runTest(format(IntegerDeclaration), false));
+        verify(runTest(format(IntegerDeclaration2), false));
     }
 
+    @Test
+    public void testEmptyFile() {
+        verify(runTest(EmtpyFile, true));
+    }
 
+    @Test
+    public void testIfStatementsFile() {
+        verify(runTest(IfStatementsFile, true));
+    }
 
+    @Test
+    public void testMargeFile() {
+        verify(runTest(MargeFile, true));
+    }
 
+    @Test
+    public void testNumberOperatorsFile() {
+        verify(runTest(NumberOperatorsFile, true));
+    }
 
+    @Test
+    public void testNumbersFile() {
+        verify(runTest(NumbersFile, true));
+    }
 
+    @Test
+    public void testReactionFile() {
+        verify(runTest(ReactionFile, true));
+    }
 
+    @Test
+    public void testReactionOperatorsFile() {
+        verify(runTest(ReactionOperatorsFile, true));
+    }
 
-
-
-
-
-
-
-
-
-
-
+    public void verify(List<String> list) {
+        if (list.size() == 0) {
+            Assert.assertTrue(list.size() == 0);
+        } else {
+            for (String s : list) {
+                System.out.println(s);
+            }
+        }
+    }
 
 
     public String format(String testStatement) {
