62b81fcf306cc232f949d0b2e592cbc6d15f80ed
GijsBeernink <g.j.beernink@student.utwente.nl>
2016-06-20 16:39:04 +0200

    Begin BottomUpListener. Making CFG

---
+++ src/donut/CFGBuilderTest.java
@@ -0,0 +1,17 @@
+package donut;
+
+import java.io.File;
+
+/**
+ * Created by Gijs on 20-Jun-16.
+ */
+public class CFGBuilderTest {
+
+    public static void main(String args[]) {
+        BottomUpCFGBuilder builder = new BottomUpCFGBuilder();
+
+
+    }
+
+
+}
